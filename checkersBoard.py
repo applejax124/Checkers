48e1b1bbcf8e370b8919e61101137305414eb5ea
Anna Katherine Smith <asmith48@student03.cse.nd.edu>
2017-04-19 00:08:13 -0400

    board

--- checkersBoard.py
+++ checkersBoard.py
@@ -1,17 +1,38 @@
-#/afs/nd.edu/user14/csesoft/cse20312/bin/python
 #!~pbui/pub/anaconda2-4.1.1/bin
 
 import pygame
 
+#dictionary with board # and coordinates
+#function to take mouse click and return board number
+
+#highlight yellow when clicked
+#show clicked piece and future move
+#send move coordinate info to game play
+
 def board():
+   #quit button
+   pygame.draw.rect(screen,(255,255,255),pygame.Rect(730,740,50,20))
+   font = pygame.font.Font(None, 24)
+   text = font.render("Quit",True,(0,0,0))
+   screen.blit(text, (737,743))
+
+   #board of squares
    colorAlt = 0
-   for x in range (52,748,87):
+   for x in range (80,720,80): 
       colorAlt+=1
-      for y in range (52,748,87):
+      for y in range (80,720,80):
 	 if (colorAlt % 2 == 0): color = (255,0,0)
 	 else: color = (0,0,255)
-         pygame.draw.rect(screen,color,pygame.Rect(x,y,87,87))
+         pygame.draw.rect(screen,color,pygame.Rect(x,y,80,80))
 	 colorAlt+=1
+
+def pieces(square):
+   for i in range(32):
+      if (): #piece is filled
+	 if i is square:
+	    pygame.draw.circle(screen,(255,255,0),dict[i],20)
+	 else:
+            pygame.draw.circle(screen,(255,255,255),dict[i],20)
 
 pygame.init()
 screen = pygame.display.set_mode((800,800))
@@ -19,18 +40,30 @@ play = True
 clock = pygame.time.Clock()
 
 board()
+pieces(33)
 
 while (play):
    for event in pygame.event.get():
-      if event.type==pygame.KEYDOWN and event.key==pygame.K_p:
-	 print pygame.mouse.get_pos()
+      if event.type==pygame.KEYDOWN and pygame.mouse.get_pressed()[0]:
+         #get board square number w func and pygame.mouse.get_pos()
 	 screen.fill((0,0,0))
 	 board()
-	 pygame.draw.circle(screen,(255,255,255),pygame.mouse.get_pos(),20)
-      #if event.type==pygame.KEYDOWN and pygame.mouse.get_pressed()[0]:
-	 #print "HEY!"
-      if event.type==pygame.KEYDOWN and event.key==pygame.K_SPACE:
+	 pieces(#board square number)
+	 if #square is between 1 and 32
+	    moveSelect = False
+	    while (!moveSelect):
+	       if event.type==pygame.KEYDOWN and pygame.mouse.get_pressed()[0]:
+	          moveSelect = True
+ 	    #send coordinates of move to game play
+	    #if move is valid change piece position in dictionary 
+	    screen.fill((0,0,0))
+	    board()
+	    pieces(33)
+	
+      (x,y) = pygame.mouse.get_pos()
+      if x >= 730 and x <= 780 and y >= 740 and y <= 760:
          play = False
+
 
       #clock.tick(60)
       pygame.display.flip()
