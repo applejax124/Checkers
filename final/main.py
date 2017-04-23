aa88ac5ac691828c1fcd47d700d122d94a1b4ef8
camicarballo <ccarball@nd.edu>
2017-04-23 12:54:00 -0400

    main.py

--- final/main.py
+++ final/main.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 
+import pygame
+import screens1
+import checkersBoard
+import gameplay
+import classes
+
+#draw home screen
+homeScreen()
+
 #create board
 game_board = board()
 
