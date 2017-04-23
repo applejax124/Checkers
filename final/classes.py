3ec2c4de2bd9d1b7b3c94596d1a3d57062f7e19b
Allison Raines <araines@student00.cse.nd.edu>
2017-04-23 12:17:48 -0400

    added range

--- final/classes.py
+++ final/classes.py
@@ -3,12 +3,12 @@
 class board:
     def __init__(self):
         b = []
-        for i in 1 2 3 4 5 6 7 8 9 10 11 12:
+        for i in range(12, 1):
             b[i] = piece('r')
             setCell(b[i],i)
-        for i in 13 14 15 16 17 18 19 20:
+        for i in range(20, 13):
             b[i] = ' '
-        for i in 21 22 23 24 25 26 27 28 29 30 31 32:
+        for i in range(32, 21):
             b[i] = piece('b')
             setCell(b[i],i)
 
